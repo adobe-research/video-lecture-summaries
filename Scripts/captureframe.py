df023ad47e5d60b61246e06ecf322b2bb46d96d8
Hijung Shin <hishin@mit.edu>
2015-01-09 10:30:48 -0500

    scripts

---
+++ Scripts/captureframe.py
@@ -0,0 +1,16 @@
+'''
+Created on Jan 7, 2015
+
+@author: hijungshin
+'''
+
+import sys
+from processvideo import ProcessVideo
+import util
+
+if __name__ == "__main__":     
+    videoname = sys.argv[1]
+    outdir = sys.argv[2]
+    fnumbers = [100,200,300]
+    pv = ProcessVideo(videoname)
+    pv.captureframes(fnumbers, outdir)
